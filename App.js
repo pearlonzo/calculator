be869a2f73567c6c8f4ae53f674023175d515d08
pearlonzo <pearlonzo@hotmail.com>
2020-09-29 13:42:15 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import './App.css';
+window.React = React
 
-import './styles.css';
-
-import Calculator from './Calculator';
-
-const app = () => (
-  <div className="app">
-    <Calculator />
-  </div>
-);
-
-export default app;
+class LoginForm extends React.Component {
+  constructor(props){
+  super(props);
+  this.state = { username: '', password: '' };
+  }
+  handleChange = ({ target }) => {
+  this.setState({ [target.name]: target.value });
+  };
+  render() {
+  return (
+  <React.Fragment>
+  <form>
+  <label htmlFor="username">username</label>
+  <input
+  type="text"
+  name="username"
+  value={this.state.username}
+  onChange={this.handleChange}
+  />
+  <label htmlFor="password">password</label>
+  <input
+  type="password"
+  name="password"
+  value={this.state.password}
+  onChange={this.handleChange}
+  />
+  </form>
+  <h3>Your username is: {this.state.username}</h3>
+  </React.Fragment>
+  );
+  }
+  }
+  export default LoginForm;  
