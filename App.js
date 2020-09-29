755a5252066b743668d18706ab0d91aaeb09414d
pearlonzo <pearlonzo@hotmail.com>
2020-09-29 13:36:54 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+import Calculator from './Calculator';
+
+const app = () => (
+  <div className="app">
+    <Calculator />
+  </div>
+);
+
+export default app;
---
+++ Screen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ResultScreen from './ResultScreen';
+import ComputationScreen from './ComputationScreen';
+
+const screen = (props) => (
+    <section className="screen">
+        <ResultScreen>{props.result}</ResultScreen>
+        <ComputationScreen>{props.equation}</ComputationScreen>
+    </section>
+);
+
+export default screen;
